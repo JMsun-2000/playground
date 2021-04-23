57c3807062f0ba0be07143e072f2e332bc366b59
Jimmy Sun <jmsun_2000@hotmail.com>
2021-04-23 16:46:25 -0400

    yolo model

---
+++ myYolo/my_yolo.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 16 15:59:52 2021
+
+@author: sunjim
+"""
+import sys
+
+from keras.models import Model
+from keras.layers import Lambda
+
+from my_utils.utils import compose
+from my_utils.keras_darknet19 import (DarknetConv2D, DarknetConv2D_BN_Leaky,
+                              darknet_body)
+
+
+def MyYolo(input_shape = (608, 608, 3)):
+    X_input = Input(input_shape)
+    # Stage 1
+    X = Conv2D(32, (1, 1), strides = (1, 1), name = 'conv2d_1', kernel_initializer = glorot_uniform(seed=0))(X)
+    
+def space_to_depth_x2(x):
+    """Thin wrapper for Tensorflow space_to_depth with block_size=2."""
+    # Import currently required to make Lambda work.
+    # See: https://github.com/fchollet/keras/issues/5088#issuecomment-273851273
+    import tensorflow as tf
+    return tf.space_to_depth(x, block_size=2)    
+
+def space_to_depth_x2_output_shape(input_shape):
+    """Determine space_to_depth output shape for block_size=2.
+
+    Note: For Lambda with TensorFlow backend, output shape may not be needed.
+    """
+    return (input_shape[0], input_shape[1] // 2, input_shape[2] // 2, 4 *
+            input_shape[3]) if input_shape[1] else (input_shape[0], None, None,
+                                                    4 * input_shape[3])
+
+def YoloBody(inputs, num_anchors, num_classes):
+    darknet = Model(inputs, darknet_body()(inputs))
+    conv20 = compose(
+        DarknetConv2D_BN_Leaky(1024, (3, 3)),
+        DarknetConv2D_BN_Leaky(1024, (3, 3))
+        )(darknet.output)
+    # layer No.43 is the last layer of conv13
+    conv13 = darknet.layers[43].output
+    conv21 = DarknetConv2D_BN_Leaky(64, (1, 1))(conv13)
+    conv21_reshaped = Lambda(
+        space_to_depth_x2,
+        output_shape=space_to_depth_x2_output_shape,
+        name='space_to_depth'
+        )(conv21)
+    x = concatenate([conv21_reshaped, conv20])
+    x = DarknetConv2D_BN_Leaky(1024, (3, 3))(x)
+    # every anchor should have classes, x, y, w, h
+    x = DarknetConv2D(num_anchors * (num_classes + 5), (1, 1))(x) 
+    return Model(inputs, x)
+
+
+
+
+
+
+
+
